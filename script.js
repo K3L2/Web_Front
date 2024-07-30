4d2c4e62432efab57d891e6cf7ceba948b65ab31
NamHyeon1 <bene301@naver.com>
2024-07-30 16:00:00 +0900

    completed ui template

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function toggleSidebar() {
     const mainContent = document.getElementById('main-content');
     const topBar = document.getElementById('top-bar');
     if (sidebar.classList.contains('open')) {
-        mainContent.style.marginLeft = '250px';
-        topBar.style.left = '250px';
+        mainContent.style.marginLeft = '150px';
+        topBar.style.left = '150px';
     } else {
         mainContent.style.marginLeft = '0';
         topBar.style.left = '0';
