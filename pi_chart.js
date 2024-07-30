450a39f22615f54042cd1492d524780168cb6043
NamHyeon1 <bene301@naver.com>
2024-07-30 16:30:48 +0900

    changed User Interface

--- pi_chart.js
+++ pi_chart.js
@@ -34,11 +34,10 @@ var option = {
                 show: false
             },
             data: [
-                { value: 1048, name: 'Search Engine' },
-                { value: 735, name: 'Direct' },
-                { value: 580, name: 'Email' },
-                { value: 484, name: 'Union Ads' },
-                { value: 300, name: 'Video Ads' }
+                { value: 500, name: 'Pattern Error' },
+                { value: 250, name: 'Ink Error' },
+                { value: 150, name: 'Scratch Error' },
+                { value: 100, name: 'Au Error' }
             ]
         }
     ]
