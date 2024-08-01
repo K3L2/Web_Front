b28b99bf66aac02e0e91601cab191a66f62dae2d
NamHyeon1 <bene301@naver.com>
2024-08-01 13:42:09 +0900

    changed bar-chart design

---
+++ bar_chart2.js
@@ -0,0 +1,82 @@
+var barChartDom = document.getElementById('barChart');
+var barChart = echarts.init(barChartDom);
+
+barChartOption = {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    legend: {},
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value'
+      }
+    ],
+    series: [
+      {
+        name: 'Pattern Error',
+        type: 'bar',
+        stack: 'Ad',
+        emphasis: {
+          focus: 'series'
+        },
+        data: [40, 13, 10, 13, 9, 21, 11],
+        barWidth: '45%',
+        itemStyle: {
+            barBorderRadius: 5,
+        }
+      },
+      {
+        name: 'Ink Error',
+        type: 'bar',
+        stack: 'Ad',
+        emphasis: {
+          focus: 'series'
+        },
+        data: [22, 18, 19, 23, 29, 10, 9],
+        itemStyle: {
+            // barBorderRadius: 3,
+        }
+      },
+      {
+        name: 'Scratch Error',
+        type: 'bar',
+        stack: 'Ad',
+        emphasis: {
+          focus: 'series'
+        },
+        data: [6, 11, 24, 17, 10, 15, 17],
+        itemStyle: {
+            // barBorderRadius: 3,
+        }
+      },
+      {
+        name: 'Au Error',
+        type: 'bar',
+        stack: 'Ad',
+        emphasis: {
+          focus: 'series'
+        },
+        data: [15, 22, 21, 14, 10, 12, 10],
+        itemStyle: {
+            barBorderRadius: 5,
+        }
+      }
+    ]
+};
+
+barChart.setOption(barChartOption);
