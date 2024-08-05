8a985736727455fcedccf817f24ecd2a0f08c5c7
NamHyeon1 <bene301@naver.com>
2024-08-05 15:01:39 +0900

    commit changes

--- bar_chart2.js
+++ bar_chart2.js
@@ -34,10 +34,12 @@ barChartOption = {
         emphasis: {
           focus: 'series'
         },
-        data: [40, 13, 10, 13, 9, 21, 11],
+        // data: [40, 13, 10, 13, 9, 21, 11],
+        data: [40],
         barWidth: '45%',
         itemStyle: {
             barBorderRadius: 5,
+            // color: '#91cc75'
         }
       },
       {
@@ -47,7 +49,8 @@ barChartOption = {
         emphasis: {
           focus: 'series'
         },
-        data: [22, 18, 19, 23, 29, 10, 9],
+        // data: [22, 18, 19, 23, 29, 10, 9],
+        data: [22],
         itemStyle: {
             // barBorderRadius: 3,
         }
@@ -59,7 +62,8 @@ barChartOption = {
         emphasis: {
           focus: 'series'
         },
-        data: [6, 11, 24, 17, 10, 15, 17],
+        // data: [6, 11, 24, 17, 10, 15, 17],
+        data: [6],
         itemStyle: {
             // barBorderRadius: 3,
         }
@@ -71,7 +75,8 @@ barChartOption = {
         emphasis: {
           focus: 'series'
         },
-        data: [15, 22, 21, 14, 10, 12, 10],
+        // data: [15, 22, 21, 14, 10, 12, 10],
+        data: [15],
         itemStyle: {
             barBorderRadius: 5,
         }
