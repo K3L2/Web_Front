6d6470a5bec408f573913937c818d2de80832198
Jinny Kim <kyj20214@gmail.com>
2024-08-06 13:46:16 +0900

    error post success

--- search/js/scripts.js
+++ search/js/scripts.js
@@ -1,20 +1,7 @@
-// js/scripts.js
-
-$(document).ready(function(){
-  // Initialize datepickers
-  $('#start-date').datepicker({
-      format: 'yyyy-mm-dd',
-      autoclose: true
-  });
-
-  $('#end-date').datepicker({
-      format: 'yyyy-mm-dd',
-      autoclose: true
-  });
-
-  // Search button click event
-  $('#searchButton').click(function() {
-      // 여기서 검색 로직을 추가하거나 구현합니다.
-      alert("Search button clicked!");
-  });
+$(document).ready(function () {
+    // Bootstrap datepicker 초기화
+    $('.datepicker-input').datepicker({
+        format: 'yyyy-mm-dd',
+        autoclose: true
+    });
 });
